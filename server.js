f427ad833104383d3701ffe6adc587cefa8648ca
Brian Gorman <briangorman99@gmail.com>
2023-08-22 15:17:01 -0400

    add server.js, setup server

---
+++ server.js
@@ -0,0 +1,14 @@
+import express, { urlencoded, json } from "express";
+import db from "./config/connection";
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running at localhost:${PORT}`);
+  });
+});
