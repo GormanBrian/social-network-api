ba96cc84b122a652ea783ab225193e1ad348a1e5
Brian Gorman <briangorman99@gmail.com>
2023-08-23 13:49:17 -0400

    add getUserById method

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,3 +8,14 @@ export const getUsers = async (_, res) => {
     res.status(500).json(err);
   }
 };
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId }).select("-__v");
+    if (!user)
+      return res.status(404).json({ message: "No user exists with that ID" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
